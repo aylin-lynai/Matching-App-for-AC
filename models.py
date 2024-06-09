e372ceed16d2f389920b45251f8af72a9aeaa39c
popmar4 <sorinel31996@gmail.com>
2024-06-09 22:08:40 +0200

    postgreSQL and login function created

--- models.py
+++ models.py
@@ -1,26 +1,36 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from flask_migrate import Migrate  # Import Migrate
 
 flask_app = Flask(__name__)
-flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://macx:MACX@localhost/matchingapp'
+flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+flask_app.config['SECRET_KEY'] = 'your_secret_key_here'
 db = SQLAlchemy(flask_app)
 
-class User(db.Model):
+# Initialize Migrate
+migrate = Migrate(flask_app, db)
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200))
+    is_admin = db.Column(db.Boolean, default=False)
 
 class Image(db.Model):
+    __tablename__ = 'image'
     id = db.Column(db.Integer, primary_key=True)
     image_path = db.Column(db.String(200), unique=True, nullable=False)
 
 class Reaction(db.Model):
+    __tablename__ = 'reaction'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     image_id = db.Column(db.Integer, db.ForeignKey('image.id'), nullable=False)
     reaction_value = db.Column(db.Integer, nullable=False)
 
-# Verschiebe db.create_all() in einen Anwendungskontext
 if __name__ == '__main__':
     with flask_app.app_context():
         db.create_all()
